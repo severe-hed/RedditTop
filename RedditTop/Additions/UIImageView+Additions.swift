94cc9700f3a229ed6140aea00cb0ea4da63bc1a5
Sergey Khliustin <severehed@gmail.com>
2019-04-12 14:53:42 +0300

    images download optimization

--- RedditTop/Additions/UIImageView+Additions.swift
+++ RedditTop/Additions/UIImageView+Additions.swift
@@ -12,34 +12,40 @@ import UIKit
 extension UIImageView {
     func loadFromURL(_ url: URL) {
         self.isHidden = false
-        let cache = URLCache.shared
-        let request = URLRequest(url: url)
-        if let response = cache.cachedResponse(for: request), let image = UIImage(data: response.data) {
-            self.image = image
+        let indicator = UIActivityIndicatorView(style: .gray)
+        self.addSubview(indicator)
+        indicator.center = CGPoint(x: self.bounds.width / 2, y: self.bounds.height / 2)
+        indicator.startAnimating()
+        self.image = nil
+        
+        let completion: (_ image: UIImage?) -> () = { [weak self] image in
+            DispatchQueue.main.async {
+                self?.image = image
+                indicator.stopAnimating()
+                indicator.removeFromSuperview()
+                self?.isHidden = image == nil
+            }
         }
-        else {
-            self.image = nil
-            let indicator = UIActivityIndicatorView(style: .gray)
-            self.addSubview(indicator)
-            indicator.center = CGPoint(x: self.bounds.width / 2, y: self.bounds.height / 2)
-            indicator.startAnimating()
-            URLSession.shared.dataTask(with: request) { (data, response, error) in
-                var resultImage: UIImage? = nil
-                
-                if let data = data, let response = response, let image = UIImage(data: data) {
-                    let cachedResponse = CachedURLResponse(response: response, data: data)
-                    cache.storeCachedResponse(cachedResponse, for: request)
-                    resultImage = image
-                }
-                
-                DispatchQueue.main.async {
-                    self.image = resultImage
-                    indicator.stopAnimating()
-                    indicator.removeFromSuperview()
-                    self.isHidden = resultImage == nil
-                }
-                
-            }.resume()
+        
+        DispatchQueue.global().async {
+            let cache = URLCache.shared
+            let request = URLRequest(url: url)
+            if let response = cache.cachedResponse(for: request), let image = UIImage(data: response.data) {
+                completion(image)
+            }
+            else {
+                URLSession.shared.dataTask(with: request) { (data, response, error) in
+                    var resultImage: UIImage? = nil
+                    
+                    if let data = data, let response = response, let image = UIImage(data: data) {
+                        let cachedResponse = CachedURLResponse(response: response, data: data)
+                        cache.storeCachedResponse(cachedResponse, for: request)
+                        resultImage = image
+                    }
+                    completion(resultImage)
+                    
+                    }.resume()
+            }
         }
     }
 }
